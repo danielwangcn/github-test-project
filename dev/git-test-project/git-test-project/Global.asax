bc41a5e5d26647b8d89a3b125a0633a53828c56d
danielwang <danielwang@DESKTOP-6HTBU13>
2016-08-03 11:41:38 +0800

    init commit

---
+++ dev/git-test-project/git-test-project/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="git_test_project.MvcApplication" Language="C#" %>
---
+++ dev/git-test-project/git-test-project/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(git_test_project.Startup))]
+namespace git_test_project
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
